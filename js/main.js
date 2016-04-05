363eb6a6f93b3767bf2d5afd64a25502be69d484
Jamie Waters <jwaters627@gmail.com>
2016-04-05 15:21:26 -0400

    trouble shooting for release

--- js/main.js
+++ js/main.js
@@ -47,9 +47,7 @@ function scrl() {
 
                 this.scrollLeft -= (delta);
                 var x = $('#photos').scrollLeft();
-                console.log(x);
-                console.log("this is the screen width " + screen.width);
-                if (x < (screen.width * .78) * 2 && x > 0) {
+                if (x < (screen.width * 0.78) * 2 && x > 0) {
                     event.preventDefault();
                     $('body').scrollTop(828);
                 }
