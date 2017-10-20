d0d21bbc3af14c015b7984ed768c0cb9d958b2ac
Jamie Waters <jwaters627@gmail.com>
2017-10-20 15:25:44 -0400

    fixed background image

--- src/components/AboutMe/aboutMe.js
+++ src/components/AboutMe/aboutMe.js
@@ -5,6 +5,7 @@ import styles from './aboutMe.scss';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 import profilePic from '../../img/profile.png';
+import sunset from '../../img/sunset.png';
 
 class AboutMe extends React.Component {
 
@@ -34,7 +35,7 @@ class AboutMe extends React.Component {
 
   render() {
     return(
-      <div className="aboutMe" id="aboutMeSection" >
+      <div className="aboutMe" id="aboutMeSection" style={{"background": "linear-gradient(0deg,rgba(0,0,0,0.6), rgba(100,100,100,0.4)), url("+sunset +")", "backgroundSize":"cover"}} >
           <img src={profilePic} />
           <p>I am a front end developer who has tried many things and failed many times before arriving at a career as a web developer. I am somebody who believes that you should never settle in life and should work hard to create the life you want all while learning as much as possible in the moment. This belief eventually led me to development, but the lessons that I learned in earning a degree in Physics, playing college soccer and then teaching allow me to problem solve efficiently while making any team I am on better. I am a builder and a problem solver who wants to create things that make peoples lives easier.</p>
       </div>
